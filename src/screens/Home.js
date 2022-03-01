f8d7ba822d3d0ecdf175909519b6ffa03a9c976f
DiegoSousaSilva <derickbenji2@gmail.com.br>
2022-02-28 23:15:31 -0300

    Stack and Tab Navigators

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,50 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useContext } from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Photo from "./Photo";
+import Chats from "./Chats";
+import { Ionicons } from "@expo/vector-icons";
+import Context from "../context/Context";
+import { Text } from "react-native";
+
+const Tab = createMaterialTopTabNavigator();
 
 const Home = () => {
+  const {
+    theme: { colors },
+  } = useContext(Context);
+
   return (
-    <View>
-      <Text>Hi i have a profile</Text>
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => {
+        return {
+          tabBarLabel: () => {
+            if (route.name === "photo") {
+              return <Ionicons name="camera" size={20} color={colors.white} />;
+            } else {
+              return (
+                <Text style={{ color: colors.white }}>
+                  {route.name.toLocaleUpperCase()}
+                </Text>
+              );
+            }
+          },
+          tabBarShowIcon: true,
+          tabBarLabelStyle: {
+            color: colors.white,
+          },
+          tabBarIndicatorStyle: {
+            backgroundColor: colors.white,
+          },
+          tabBarStyle: {
+            backgroundColor: colors.foreground,
+          },
+        };
+      }}
+      initialRouteName="chats"
+    >
+      <Tab.Screen name="photo" component={Photo} />
+      <Tab.Screen name="chats" component={Chats} />
+    </Tab.Navigator>
   );
 };
 
